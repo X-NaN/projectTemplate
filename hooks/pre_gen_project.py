bfdffefcde7a75e559dcb4b057414bdceda7d58a
xingnn <xingnn@tuya.com>
2022-01-29 17:16:21 +0800

    add cookiecutter hooks

---
+++ hooks/pre_gen_project.py
@@ -0,0 +1,17 @@
+import sys
+import re
+
+
+def is_module_name_valid(module_name):
+    pattern = re.compile(r"^[a-zA-Z_][a-zA-Z0-9-_]{,20}$")
+    return pattern.match(module_name)
+
+
+if __name__ == '__main__':
+    module_name = '{{cookiecutter.yourProjectName}}'
+    if is_module_name_valid(module_name):
+        print("Module name `{{cookiecutter.yourProjectName}}` is valid.")
+    else:
+        print("Module name must startswith num|letter|_ and fill with num|letter|_|- and no more than 21 length.")
+        sys.exit(1)
+    sys.exit(0)
